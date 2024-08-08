2c7b0a66c150d4c244bd709e62fa577ab68ed521
hatem-marzougui <hatem.adresse@gmail.com>
2024-08-08 15:55:20 +0200

    [back] prepare getCompartiment API

--- src/main/java/com/example/ims/ImsApplication.java
+++ src/main/java/com/example/ims/ImsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ImsApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/ims/dao/CompartimentRepository.java
@@ -0,0 +1,14 @@
+package com.example.ims.dao;
+
+import com.example.ims.model.Compartiment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CompartimentRepository extends JpaRepository<Compartiment, Integer> {
+
+
+
+
+
+}
